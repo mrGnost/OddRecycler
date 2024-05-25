30e1bf0a8ff28e3f0e94af6edbcf8d13a84dfaca
mrGnost <mrgnost@yandex.ru>
2024-05-25 12:58:56 +0300

    Changed ElementsStore behavior to remove elements immediately and thread-safely

--- app/src/main/java/com/example/oddrecycler/data/elements/ElementsStore.kt
+++ app/src/main/java/com/example/oddrecycler/data/elements/ElementsStore.kt
@@ -1,47 +1,60 @@
 package com.example.oddrecycler.data.elements
 
+import android.util.Log
 import com.example.oddrecycler.data.entity.Element
+import com.example.oddrecycler.util.Dispatcher
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class ElementsStore @Inject constructor(externalScope: CoroutineScope) {
+@Singleton
+class ElementsStore @Inject constructor(
+    externalScope: CoroutineScope,
+    dispatcher: Dispatcher
+) {
     private val data = mutableListOf<Element>()
+    private val mutex = Mutex()
 
-    private val _elements = flow<List<Element>> {
-        while (true) {
-            emit(data.apply {
-                add((0..size).random(), ElementGenerator.next())
-            })
-            delay(5000)
-        }
-    }.shareIn(externalScope, replay = 1, started = SharingStarted.WhileSubscribed())
+    private val _elements = MutableSharedFlow<List<Element>>(replay = 1)
+
     val elements: SharedFlow<List<Element>>
         get() = _elements
 
     init {
-        externalScope.launch(Dispatchers.Default) {
+        externalScope.launch(dispatcher.default) {
             startGenerator()
         }
     }
 
-    suspend fun startGenerator() {
-
+    private suspend fun startGenerator() {
+        while (true) {
+            mutex.withLock {
+                _elements.emit(data.apply {
+                    add((0..size).random(), ElementGenerator.next())
+                    Log.d("ELEMENTS_STORE", "New data: $this")
+                })
+            }
+            delay(5000)
+        }
     }
 
-    fun removeAt(id: Int) {
-        data.apply {
-            indexOfFirst { element ->
-                element.id == id
-            }.let { index ->
-                apply {
-                    removeAt(index)
+    suspend fun removeAt(id: Int) {
+        mutex.withLock {
+            data.apply {
+                Log.d("ELEMENTS_STORE", "data: $this, removing: $id")
+                indexOfFirst { element ->
+                    element.id == id
+                }.let { index ->
+                    apply {
+                        removeAt(index)
+                        _elements.emit(this)
+                    }
                 }
             }
         }
